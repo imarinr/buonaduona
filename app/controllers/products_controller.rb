9f4d2c7735b0854b26aa94f5848c41faada3dd0d
Ivan Marin <imarinr@outlook.com>
2022-07-15 00:56:48 -0500

    se tiene listo el formulario de edicion de productos, se modifico la vista index para productos para mostrar el stock disponible.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,22 @@
 class ProductsController < ApplicationController
     def index
+        @products = Product.all
+    end
+
+    def new
     end
 
     def create
     end
 
     def update
+        redirect_to controller: 'products', action: 'index'
     end
 
     def delete
     end
 
     def edit
+        @product = Product.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   post '/cart', to: 'store#add_to_cart'
 
   # products controller
-  get '/products/new' to: "products#edit" # HTML formulario de creacion
-  post '/products' to: "products#create" # orden de crear nuevo producto
-  get '/products' to: "products#index" # HTML mostrar todos los productos
-  get '/products/:id/edit' to: "products#edit" # HTML formulario de edicion
-  patch '/products/:id' to: "products#update" # orden de actualizar un producto
+  get '/products/new', to: "products#new" # HTML formulario de creacion
+  post '/products', to: "products#create" # orden de crear nuevo producto
+  get '/products', to: "products#index" # HTML mostrar todos los productos
+  get '/products/:id/edit', to: "products#edit" # HTML formulario de edicion
+  patch '/products/', to: "products#update" # orden de actualizar un producto
   delete 'products/:id', to: 'products#delete' # orden de eliminar un producto
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
