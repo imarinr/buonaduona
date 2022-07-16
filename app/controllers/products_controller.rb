1eff09804a78efafcd2968b4771cfd0cb280fc58
Ivan Marin <imarinr@outlook.com>
2022-07-15 23:28:01 -0500

    se creo la funcionalidad completa del CRUD de productos, debo decir que no es muy correcta sin embargo, parece funcionar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,51 @@
 class ProductsController < ApplicationController
+
     def index
         @products = Product.all
     end
 
+    # lo utilizo como metodo auxiliar de destroy ya que no he descubierto como hacer que funcione
+    def show
+        if params[:method] == 'delete' && !Product.find(params[:id]).nil?
+            destroy
+        else
+            redirect_to index_products_path
+        end
+    end
+
     def new
-    end
-
-    def create
-    end
-
-    def update
-        redirect_to controller: 'products', action: 'index'
-    end
-
-    def delete
+        @product = Product.new
     end
 
     def edit
         @product = Product.find(params[:id])
+    end
+
+    def create
+        @product = Product.create product_params_create
+        redirect_to index_products_path
+    end
+
+    def update
+        @product = Product.find(product_params_update[:id])
+        @product.update product_params_update
+        redirect_to index_products_path
+    end
+
+    def destroy
+        puts "\n\n\n-------Destroy\n----------"
+        Product.find(params[:id]).delete
+        redirect_to index_products_path
+    end
+
+
+    private
+
+    def product_params_update
+        params.require(:product).permit(:id, :name, :description, :price, :stock, :available)
+    end
+
+    def product_params_create
+        params.require(:product).permit(:name, :description, :price, :stock, :available)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
   get '/cart', to: 'store#cart'
   post '/cart', to: 'store#add_to_cart'
 
-  # products controller
-  get '/products/new', to: "products#new" # HTML formulario de creacion
-  post '/products', to: "products#create" # orden de crear nuevo producto
-  get '/products', to: "products#index" # HTML mostrar todos los productos
-  get '/products/:id/edit', to: "products#edit" # HTML formulario de edicion
-  patch '/products/', to: "products#update" # orden de actualizar un producto
-  delete 'products/:id', to: 'products#delete' # orden de eliminar un producto
+  # product routes
+  get 'products/index', to: 'products#index', as: 'index_products' # HTML que muestra los productos
+  get 'products/:id/edit', to: 'products#edit', as: 'edit_product' # HTML para editar un producto
+  get 'products/new', to: 'products#new', as: 'new_product' # HTML para nuevo producto
+  get 'products/:id', to: 'products#show', as: 'show_product' # parece necesario
+
+  post 'products/', to: 'products#create', as: 'products'
+  patch 'products', to: 'products#update', as: 'product'
+  delete 'products/:id', to: 'products#destroy'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
