5296853ae0eb368d692cde94172d4cdc7ce36e0d
Ivan Marin <imarinr@outlook.com>
2022-07-24 09:48:10 -0500

    Se detectó y solucionó un problema con la instalación de bibliotecas JavaScript dentro del proyecto. Debido a este programa el metodo DELETE era ignorado en las solicitudes. Ahora, el CRUD funciona correctamente

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,9 @@
 class ProductsController < ApplicationController
 
+    # render layout: 'layouts/backstage'
+
     def index
         @products = Product.all
-    end
-
-    # lo utilizo como metodo auxiliar de destroy ya que no he descubierto como hacer que funcione
-    def show
-        if params[:method] == 'delete' && !Product.find(params[:id]).nil?
-            destroy
-        else
-            redirect_to index_products_path
-        end
     end
 
     def new
@@ -21,31 +14,32 @@ class ProductsController < ApplicationController
         @product = Product.find(params[:id])
     end
 
+    def show
+        redirect_to products_path
+    end
+
     def create
-        @product = Product.create product_params_create
-        redirect_to index_products_path
+        @product = Product.create product_params
+        redirect_to products_path
     end
 
     def update
-        @product = Product.find(product_params_update[:id])
-        @product.update product_params_update
-        redirect_to index_products_path
+        @product = Product.find(params[:id])
+        @product.update product_params
+        redirect_to products_path
     end
 
     def destroy
         puts "\n\n\n-------Destroy\n----------"
-        Product.find(params[:id]).delete
-        redirect_to index_products_path
+        @product = Product.find(params[:id])
+        @product.destroy
+        redirect_to products_path
     end
 
 
     private
 
-    def product_params_update
-        params.require(:product).permit(:id, :name, :description, :price, :stock, :available)
-    end
-
-    def product_params_create
+    def product_params
         params.require(:product).permit(:name, :description, :price, :stock, :available)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,7 @@ Rails.application.routes.draw do
   post '/cart', to: 'store#add_to_cart'
 
   # product routes
-  get 'products/index', to: 'products#index', as: 'index_products' # HTML que muestra los productos
-  get 'products/:id/edit', to: 'products#edit', as: 'edit_product' # HTML para editar un producto
-  get 'products/new', to: 'products#new', as: 'new_product' # HTML para nuevo producto
-  get 'products/:id', to: 'products#show', as: 'show_product' # parece necesario
-
-  post 'products/', to: 'products#create', as: 'products'
-  patch 'products', to: 'products#update', as: 'product'
-  delete 'products/:id', to: 'products#destroy'
+  resources :products
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
